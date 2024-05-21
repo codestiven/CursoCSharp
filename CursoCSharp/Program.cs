279cee7659af148e5ff99937824802ddaa7cd74a
stiven de la rosa brito <codestiven@gmail.com>
2024-05-21 14:27:39 -0400

    10. Constantes

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,10 +1,4 @@
-﻿// comentario de una sola linea
-/* comentario de varias lineas */
-
-int num = 50;
-double num2 = 2.5;
-Console.WriteLine(num/num2);
-Console.WriteLine(Math.Abs(-5));
-
-Console.WriteLine(Math.Pow(2,5));
+﻿const int edad = 25;
+const String nombre = "Pablo";
+Console.WriteLine(edad);
 
