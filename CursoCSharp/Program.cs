714bdce12f62ea16dcb6f46572a9681dd832904f
stiven de la rosa brito <codestiven@gmail.com>
2024-05-21 14:23:07 -0400

    9. Comentarios

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,4 +1,7 @@
-﻿int num = 50;
+﻿// comentario de una sola linea
+/* comentario de varias lineas */
+
+int num = 50;
 double num2 = 2.5;
 Console.WriteLine(num/num2);
 Console.WriteLine(Math.Abs(-5));
