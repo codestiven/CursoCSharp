ebc3a4531400d5c8882c4bb7813a0f5c7e86d607
stiven de la rosa brito <codestiven@gmail.com>
2024-05-21 13:49:34 -0400

    Mostrar información al usuario

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,9 +1,6 @@
-﻿int numeroEntero = 42;
-double numeroDoble = 3.141592653589793;
-float numeroFlotante = 3.14f;
-char caracter = 'A';
-bool esVerdadero = true;
-string texto = "Hola, Mundo!";
-DateTime fechaActual = DateTime.Now;
+﻿string texto = "Hola, Mundo!";
+
+Console.WriteLine(texto + "\n" + texto + " \" cositas\\  \" ");
+
 
 
