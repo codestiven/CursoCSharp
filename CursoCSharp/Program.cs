aca167035108042714d91f1bb6bb8df772887160
stiven de la rosa brito <codestiven@gmail.com>
2024-05-21 13:42:45 -0400

    Tipos de datos

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,3 +1,9 @@
-﻿String cocacola = "aaa";
-int popo = 50;
-Console.WriteLine("Hola, " + cocacola + " " + popo);
+﻿int numeroEntero = 42;
+double numeroDoble = 3.141592653589793;
+float numeroFlotante = 3.14f;
+char caracter = 'A';
+bool esVerdadero = true;
+string texto = "Hola, Mundo!";
+DateTime fechaActual = DateTime.Now;
+
+
