0ffa82037cc4338c9e7c4577f612d26c5e7520cb
stiven de la rosa brito <codestiven@gmail.com>
2024-05-22 07:56:13 -0400

    23. Declarar clases y objetos

---
+++ CursoCSharp/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+    internal class Alumno
+    {
+        public string nombre;
+        public int edad = 20;
+        public double nota;
+        public string grado;
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,5 +1,10 @@
-﻿int[] ints = { 1, 2, 3, 4, 5, 7 };
-for (int i = 0; i < ints.Length; i++)
-{
-    Console.WriteLine(ints[i]);
-}
+﻿
+using CursoCSharp;
+
+Alumno a1 = new Alumno();
+a1.nombre = "juan";
+a1.edad = 25;
+a1.nota = 5.2;
+a1.grado = "software";
+
+Console.WriteLine($"Nombre: {a1.nombre}, Edad: {a1.edad}, Nota: {a1.nota}, Grado: {a1.grado}");
